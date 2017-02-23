44e2af6dd9da41a1e823e780834e3eb396faa298
Chaparro25 <chaparroreyes@gmail.com>
2017-02-23 14:01:30 -0600

    Create EjemploD.js

---
+++ EjemploD.js
@@ -0,0 +1,34 @@
+//Para formar una malla por revolución, debe establecer los puntos que definirán la forma de la malla.
+
+//Para ello, primero se define un arreglo vacío
+
+var puntos = [];
+//Se insertan 50 puntos en el arreglo con diversos valores.
+
+for ( var i = 0; i < 50; i ++ ) {
+    puntos.push( new THREE.Vector2(
+                     Math.sin( i * 0.2 ) * 15 + 50,
+                     ( i - 5 ) * 2 ) );
+}
+
+//Se definen la forma, el material y la malla.
+
+var forma = new THREE.LatheGeometry(puntos);
+
+var material = new THREE.MeshNormalMaterial();
+
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX( Math.PI/6 );
+
+//Se completa el código.
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
